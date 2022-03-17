48a5b47d3f38c9f2b238eef8392a5591ffa85b86
syedf <syedf@Heera>
2022-03-17 15:54:42 -0700

    Testing jenkins for the first time

--- src/test/wellcome.java
+++ src/test/wellcome.java
@@ -5,6 +5,8 @@ public class wellcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("this is my project");
+     
+     System.out.println("We are testing jenkins integration with github");
 	}
 
 }
